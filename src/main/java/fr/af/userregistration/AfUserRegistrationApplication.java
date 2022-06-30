32beb8f0b4eca31614e58b52c4bed69b9efe8e64
FatmaBenHamda <fatma.hamda@aymax.fr>
2022-06-30 09:35:22 +0100

    init project

---
+++ src/main/java/fr/af/userregistration/AfUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package fr.af.userregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfUserRegistrationApplication.class, args);
+	}
+
+}
